00f90a421110c8ef1f26fcbbb414288da65d13a9
Dyzing <58467833+Dyzing@users.noreply.github.com>
2020-02-21 15:52:34 +0100

    Add files via upload

--- TD4/actions.c
+++ TD4/actions.c
@@ -1,7 +1,7 @@
 #include "actions.h"
 
-extern GLfloat xrot;   
-extern GLfloat yrot; 
+extern GLfloat xrot;
+extern GLfloat yrot;
 extern int blend;
 extern int light;
 extern GLfloat angle2;
@@ -12,105 +12,104 @@ extern GLfloat angle5;
 
 float plus_angle(float angle)
 {
-	if ((0 <= angle) && (angle <  90))
-		angle += 10;
-	return angle;
+        if ((0 <= angle) && (angle <  90))
+                angle += 10;
+        return angle;
 }
 
 float moins_angle(float angle)
 {
-	if ((0 < angle) && (angle <= 90))
-		angle -=10;
-	return angle;
+        if ((0 < angle) && (angle <= 90))
+                angle -=10;
+        return angle;
 }
 
 
 
-void touche_pressee(unsigned char key, int x, int y) 
+void touche_pressee(unsigned char key, int x, int y)
 {
     usleep(100);
 
-    switch (key) {    
-    case ESCAPE: 
-	exit(1);                   	
-	break; 
+    switch (key) {
+    case ESCAPE:
+        exit(1);
+        break;
 
     case ESPACE:
       xrot = 0.0f;
       yrot = 0.0f;
       break;
 
-    case TOUCHE_MIN_B: 
-    case TOUCHE_MAJ_B: 
+    case TOUCHE_MIN_B:
+    case TOUCHE_MAJ_B:
      blend =  switch_blend(blend);
       break;
 
-    case TOUCHE_MIN_L: 
-    case TOUCHE_MAJ_L: 
+    case TOUCHE_MIN_L:
+    case TOUCHE_MAJ_L:
       light = switch_light(light);
       break;
- /*     
+
     case TOUCHE_MAJ_A:
-   		angle2 = moins_angle(angle2);
-		break;
+                angle2 = moins_angle(angle2);
+                break;
     case TOUCHE_MIN_A:
-		angle2 = plus_angle(angle2);
-		break;
-		
+                angle2 = plus_angle(angle2);
+                break;
+
     case TOUCHE_MAJ_Z:
-		angle3 = moins_angle(angle3);
-		break;
+                angle3 = moins_angle(angle3);
+                break;
     case TOUCHE_MIN_Z:
-		angle3 = plus_angle(angle3);
-		break;
-		
+                angle3 = plus_angle(angle3);
+                break;
+
     case TOUCHE_MAJ_E:
-		angle4 = moins_angle(angle4);
-		break;
+                angle4 = moins_angle(angle4);
+                break;
     case TOUCHE_MIN_E:
-		angle4 = plus_angle(angle4);
-		break;
-		
+                angle4 = plus_angle(angle4);
+                break;
+
     case TOUCHE_MAJ_R:
-		angle5 = moins_angle(angle5);
-		break;
+                angle5 = moins_angle(angle5);
+                break;
     case TOUCHE_MIN_R:
-		angle5 = plus_angle(angle5);
-		break;
-		*/
-    }	
+                angle5 = plus_angle(angle5);
+                break;
+    }
 }
 
 void touche_speciale(int touche, int x, int y)
 {
-	 usleep(100);
-    switch (touche) { 
-			
-		case GLUT_KEY_PAGE_UP: 
-		  xrot += 5.0f;		
-			break;
-			
-		case GLUT_KEY_PAGE_DOWN:
-		  xrot -= 5.0f;		
-			break;
+         usleep(100);
+    switch (touche) {
 
-		case GLUT_KEY_UP:
-		  xrot += 1.0f;		
-			break;
-			
-		case GLUT_KEY_DOWN:
-		  xrot -= 1.0f;		
-			break;
-			
-		case GLUT_KEY_LEFT:
-		  yrot += 1.0f;
-			break;
+                case GLUT_KEY_PAGE_UP:
+                  xrot += 5.0f;
+                        break;
 
-		case GLUT_KEY_RIGHT:
-		  yrot -= 1.0f;
-			break;
-	}	
-	
+                case GLUT_KEY_PAGE_DOWN:
+                  xrot -= 5.0f;
+                        break;
+
+                case GLUT_KEY_UP:
+                  xrot += 1.0f;
+                        break;
+
+                case GLUT_KEY_DOWN:
+                  xrot -= 1.0f;
+                        break;
+
+                case GLUT_KEY_LEFT:
+                  yrot += 1.0f;
+                        break;
+
+                case GLUT_KEY_RIGHT:
+                  yrot -= 1.0f;
+                        break;
+        }
+
 }
 
 
--- TD4/main.c
+++ TD4/main.c
@@ -11,12 +11,11 @@ GLfloat z = -5.0f;
 int blend = 0;
 int light = 0;
 
-
 GLfloat angle2 = 0;
 GLfloat angle3 = 0;
 GLfloat angle4 = 0;
 GLfloat angle5 = 0;
-GLfloat angle_triangle = 0;
+
 // 0 : avant, 1 : arriere, 2 : droit, 3 : gauche, 4 : dessus, 5 : dessous
 static int faces[6][4] = {{0, 1, 2, 3},
 						 {4, 5, 6, 7},
@@ -80,257 +79,36 @@ struct cube1 creer_cube(float taille)
 
 void affiche_cube1(struct cube1 mon_cube)
 {
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity( );
-
-    gluLookAt(3,4,2,0,0,0,0,0,1);
-
+    int k = 0;
     glBegin(GL_QUADS);
     
     for (int i = 0; i < 6; i++) 
     {
 		for (int j = 0; j < 4; j++)
 		{
+                        k = i/(j+1);
 			glVertex3f(mon_cube.tablo[faces[i][j]][0], mon_cube.tablo[faces[i][j]][1], mon_cube.tablo[faces[i][j]][2]); 
-		}
+                        glColor3f(i,j,k);
+                }
 	}
 	glEnd();
 }
     
-    	
-	/*
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-    glMatrixMode( GL_MODELVIEW );
-    glLoadIdentity( );
-
-    gluLookAt(3,4,2,0,0,0,0,0,1);
-
-    glBegin(GL_QUADS);
-
-    glColor3ub(255,0,0); //face rouge
-    glVertex3d(1,1,1);
-    glVertex3d(1,1,-1);
-    glVertex3d(-1,1,-1);
-    glVertex3d(-1,1,1);
-
-	glColor3ub(0,255,0); //face verte
-    glVertex3d(1,-1,1);
-    glVertex3d(1,-1,-1);
-    glVertex3d(1,1,-1);
-    glVertex3d(1,1,1);
-    
-	glColor3ub(0,0,255); //face bleue
-    glVertex3d(-1,-1,1);
-    glVertex3d(-1,-1,-1);
-    glVertex3d(1,-1,-1);
-    glVertex3d(1,-1,1);
-
-    glColor3ub(255,255,0); //face jaune
-    glVertex3d(-1,1,1);
-    glVertex3d(-1,1,-1);
-    glVertex3d(-1,-1,-1);
-    glVertex3d(-1,-1,1);
-
-    glColor3ub(0,255,255); //face cyan
-    glVertex3d(1,1,-1);
-    glVertex3d(1,-1,-1);
-    glVertex3d(-1,-1,-1);
-    glVertex3d(-1,1,-1);
-
-    glColor3ub(255,0,255); //face magenta
-    glVertex3d(1,-1,1);
-    glVertex3d(1,1,1);
-    glVertex3d(-1,1,1);
-    glVertex3d(-1,-1,1);
-    
-    glEnd();
-    
-    glFlush();
-    glutSwapBuffers();
-    
-    */
-
-	
 
 
 GLvoid Modelisation()
 {
   VM_init();
-  	glEnable(GL_DEPTH_TEST);
-	
-  // Entre glPushMatrix et glPopMatrix s'écrit la description de la scène.
-/*
-	glPushMatrix();
-	{
-		glRotatef(angle_triangle, 0, 1, 0);
-		angle_triangle = angle_triangle+1;
-		glBegin(GL_TRIANGLES);
-			glVertex3f(0.0, 2.0, 0.0);
-			glVertex3f(1.0, -1.0, 0.0);
-			glVertex3f(-1.0, -1.0, 0.0);
-		glEnd();
-	
-	}
-	glPopMatrix();
-
-	printf ("%f \n",GLUT_ELAPSED_TIME) ;
-	* */
-/*
- 
-
   glPushMatrix();
   {
-    // Ceci, qui n'utilise pas les primitives de calcul d'OpenGL
-    GLdouble translate[16] = {2.25,0,0,0, //paume
-                              0,3.5,0,0,
-                              0,0,0.5,0,
-                              0,0,0,1};
-    glMultMatrixd(translate);
-    glutSolidCube(0.5);
-}
-	glPopMatrix();
+  	glEnable(GL_DEPTH_TEST);
 	
 
-    //------------------------doigt 5-------------------------------------//
-	glPushMatrix();
-	{
-	glTranslatef(0,0.85,0);
-	glRotatef(angle5,1,0,0);
-	glTranslatef(0,-0.85,0);
-	
-    glColor3f(1, 0, 0);
+	affiche_cube1(creer_cube(1));
 
-    glTranslatef(-0.45, 1.1, 0); //phalange 1
-		glScalef(0.5, 0.8, 0.2);
-		glutSolidCube(0.5);
-    
-    glColor3f(0.8, 0.2, 0);
-    		
-    glTranslatef(0, 0.5, 0); //phalange 2
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-    
-    glColor3f(0.6, 0.4, 0);
-    		
-    glTranslatef(0, 0.5, 0); //phalange 3
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-    
-}
-	glPopMatrix();
-	
-
-
-// ---------------------------doigt 4--------------------------------------//
-	glPushMatrix();
-	{
-	glTranslatef(0,0.85,0);
-	glRotatef(angle4,1,0,0);
-	glTranslatef(0,-0.85,0);
-	
-    glColor3f(0, 1, 0);
-
-
-    glTranslatef(-0.15, 1.1, 0); //phalange 1
-		glScalef(0.5, 1, 0.2);
-		glutSolidCube(0.5);
-
-    glColor3f(0, 1.4, 0.5);
-
-
-    glTranslatef(0, 0.5, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-		
-    glColor3f(0, 0.2, 0.4);
-
-
-    glTranslatef(0, 0.5, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-
-
-
-}
-
-	glPopMatrix();
-	
-
-// ---------------------------doigt 3--------------------------------------//
-	
-	glPushMatrix();
-	{
-	glTranslatef(0,0.85,0);
-	glRotatef(angle3,1,0,0);
-	glTranslatef(0,-0.85,0);
-			
-    glColor3f(0.2, 0.2, 1.2);
-		
-
-    glTranslatef(0.15, 1.1, 0); //phalange 1
-		glScalef(0.5, 1.3, 0.2);
-		glutSolidCube(0.5);
-		
-    glColor3f(0.2, 0.4, 1.6);
-
-		
-    glTranslatef(0, 0.48, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-		
-		
-    glColor3f(1, 0.2, 0.1);
-
-
-    glTranslatef(0, 0.48, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-		
-
-	}
-
-	glPopMatrix();
-		
-// ---------------------------doigt 2--------------------------------------//
-	glPushMatrix();
-	{
-	glTranslatef(0,0.85,0);
-	glRotatef(angle2,1,0,0);
-	glTranslatef(0,-0.85,0);
-			
-    glColor3f(1.4, 1, 0);
-
-
-    glTranslatef(0.45, 1.1, 0); //phalange 1
-		glScalef(0.5, 1, 0.2);
-		glutSolidCube(0.5);
-		
-		
-    glColor3f(1.2, 1.4, 1.4);
-
-
-    glTranslatef(0, 0.5, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-
-    glColor3f(1.2, 0.2, 1.8);
-
-		
-    glTranslatef(0, 0.5, 0); //phalange 1
-		glScalef(1, 0.7, 1);
-		glutSolidCube(0.5);
-		
-    
   }
   glPopMatrix();
 
-*/
-	affiche_cube1(creer_cube(1));
-
-	
 glClearColor(0.5, 0.5, 1, 0.8);
 
 
