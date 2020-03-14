0f0973671829af6867d40b0059a0800c840dbe49
Dyzing <58467833+Dyzing@users.noreply.github.com>
2020-03-14 23:35:05 +0100

    Add files via upload

--- COLIBRI PROJECT/VM_init.c
+++ COLIBRI PROJECT/VM_init.c
@@ -6,7 +6,7 @@ void VM_init()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode( GL_MODELVIEW );
 	glLoadIdentity();
-	glTranslatef(0,0,-25);	
+	glTranslatef(0,0,-60);	
 	glRotatef(xrot, 1,0,0);
 	glRotatef(yrot, 0, 1, 0);
 }
--- COLIBRI PROJECT/main.c
+++ COLIBRI PROJECT/main.c
@@ -19,17 +19,26 @@ GLfloat angle4 = 0;
 GLfloat angle5 = 0;
 
 GLfloat angle_triangle = 0;
-
-float ambientLightDefault[] = { 0.1f, 0.1f, 0.1f, 0.1f };
-float mipuissance[] = { 1.0f, 1.0f, 1.0f, 1.0f };
    
 
 
 GLvoid Modelisation()
 {
   VM_init();      
+
+
+
   glPushMatrix();
   {
+  		//lumiere
+
+
+
+	  //transparence
+	glEnable(GL_BLEND); 
+  	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	  	//rotation
 		glRotatef(angle_triangle, 0, 1, 0);
 		angle_triangle = angle_triangle+1;
 		glTranslatef(0,0,10);
@@ -179,7 +188,146 @@ GLvoid Modelisation()
 	}
 	glPopMatrix();
 
-glClearColor(0.43, 0.57, 0, 0.8);
+
+	glPushMatrix(); //sol
+	{
+		glTranslatef(0,-20,0);
+		glScalef(20,1,20);
+		glColor3f(0.43, 0.57, 0);
+		glutSolidCube(3.0);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+
+
+
+	glPushMatrix(); //pollen
+	{
+		glTranslatef(24,2,-22.5);
+		glRotatef(-90,0,0,1);
+		glColor3f(1,1,0.34);
+		glScalef(1,1,1);
+		glutSolidSphere(0.5, 25, 25);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+
+
+
+
+	glPushMatrix(); //fleur
+	{
+		glEnable(GL_DEPTH_TEST);
+		glTranslatef(20,0.5,-20);
+		glRotatef(120,0,1,0);
+		glRotatef(-20,1,0,0);
+		glScalef(5,5,5);
+		glColor4f(1,0.71, 0.75, 0.4);
+		GLUquadric* params = gluNewQuadric(); //création du quadrique
+    	gluQuadricTexture(params,GL_TRUE); 
+	    gluCylinder(params,0.5,0,1.6,20,1); //cône 1
+        gluDeleteQuadric(params); //je supprime le quadrique
+	}
+	glPopMatrix();
+
+
+	glPushMatrix(); //tige
+	{
+		glTranslatef(26,-9.5,-23.25);
+		glRotatef(90,0,0,1);
+		glColor3f(0.19,0.8,0.19);
+		glScalef(25,1,1);
+		glutSolidSphere(0.5,25,25);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+
+
+	glPushMatrix(); //feuille droite
+	{
+		glTranslatef(24,-6,-23.25);
+		glRotatef(-135,0,0,1);
+		glColor3f(0,0.51,0);
+		glScalef(1,2,2);
+		glutSolidSphere(1, 25, 25);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+
+	glPushMatrix(); //feuille gauche
+	{
+		glTranslatef(28,-12,-23.25);
+		glRotatef(135,0,0,1);
+		glColor3f(0,0.51,0);
+		glScalef(1,2,2);
+		glutSolidSphere(1, 25, 25);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+
+
+
+	glPushMatrix(); //caillou gauche 1
+	{
+		glTranslatef(-15,-16,-15);
+		glRotatef(110,0,0,1);
+		glColor3f(0.41,0.41,0.41);
+		glScalef(1,2,2);
+		glutSolidSphere(3, 10, 10);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+	glPushMatrix(); //caillou gauche 2
+	{
+		glTranslatef(-13,-16,-15);
+		glRotatef(-110,0,0,1);
+		glColor3f(0.41,0.41,0.41);
+		glScalef(1,2,2);
+		glutSolidSphere(3, 10, 10);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+	glPushMatrix(); //caillou gauche 3
+	{
+		glTranslatef(-14,-18,-16);
+		glRotatef(-110,0,0,1);
+		glRotatef(90,0,0,1);
+		glColor3f(0.41,0.41,0.41);
+		glScalef(1,2,3);
+		glutSolidSphere(3, 10, 10);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+	glPushMatrix(); //caillou gauche 4
+	{
+		glTranslatef(-14,-18,-10);
+		glRotatef(-110,0,0,1);
+		glRotatef(-90,0,1,0);
+		glColor3f(0.41,0.41,0.41);
+		glScalef(1,2,3);
+		glutSolidSphere(2, 10, 10);
+		glEnd();
+	}
+	glPopMatrix();
+
+
+glClearColor(0.53, 0.8, 0.92, 0.8);
 
 
 
